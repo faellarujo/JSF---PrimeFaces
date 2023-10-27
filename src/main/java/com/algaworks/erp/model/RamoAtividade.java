3f284453c2d23878609f4a1910135a6b2e6dc3b7
faellarujo <raphaelaraujo.ti@gmail.com>
2023-10-27 18:44:40 +0100

    mapeamento da tabelma ramo atividade

--- src/main/java/com/algaworks/erp/model/RamoAtividade.java
+++ src/main/java/com/algaworks/erp/model/RamoAtividade.java
@@ -1,6 +1,7 @@
 package com.algaworks.erp.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 import java.io.Serializable;
@@ -11,4 +12,7 @@ import java.io.Serializable;
 @Entity
 @Table(name = "ramo_atividade")
 public class RamoAtividade implements Serializable {
+
+    @Id
+    private long id;
 }
