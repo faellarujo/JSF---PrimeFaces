ac80d686eafb098bc21725b860cc96b853b2e5b4
faellarujo <raphaelaraujo.ti@gmail.com>
2023-11-03 18:04:15 +0000

    Limpando e iniciando de verdade o projeto

--- src/main/java/com/algaworks/erp/controller/GestaoEmpresasBean.java
+++ src/main/java/com/algaworks/erp/controller/GestaoEmpresasBean.java
@@ -1,9 +1,6 @@
 package com.algaworks.erp.controller;
 
-import com.algaworks.erp.model.Empresa;
-import com.algaworks.erp.model.TipoEmpresa;
 import jakarta.faces.view.ViewScoped;
-import jakarta.inject.Inject;
 import jakarta.inject.Named;
 
 import java.io.Serializable;
@@ -13,40 +10,7 @@ import java.io.Serializable;
 @ViewScoped
 public class GestaoEmpresasBean implements Serializable {
 
-    private int numero = 0;
 
-
-    private Empresa empresa = new Empresa();
-
-
-    private TipoEmpresa tipoEmpresa;
-
-    public TipoEmpresa[] getTipoEmpresa() {
-        return TipoEmpresa.values();
-    }
-
-    public Empresa getEmpresa() {
-        return empresa;
-    }
-
-    private GestaoEmpresasBean(){
-        numero++;
-    }
-
-    public int getNumero() {
-        return numero;
-    }
-
-    public String ajuda(){
-        return "Ajuda";
-    }
-
-
-    public void salvar(){
-        System.out.println("Razao Social: " + empresa.getRazaoSocial()
-        + "  - Nome Fantasia: " + empresa.getNomeFantasia()
-        + "  - Tipo Empresa:  " + empresa.getTipo());
-    }
 
 
 }
